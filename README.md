e1fd7bcbda447be9169427945d9007fa736728f5
xh321 <xiaohe321@outlook.com>
2024-02-16 01:37:51 +0800

    支持自定义浏览器打开链接

--- README.md
+++ README.md
@@ -6,18 +6,15 @@ LiteLoaderQQNT插件，用于点击连接时自动跳转，而不用经过PCQQ�
 ## 使用方法
 
 *建议从`LiteLoaderQQNT`应用商店中直接下载安装，方便快捷。*（新版本1.0`LiteLoaderQQNT`没有插件商店了，请遵循[手动安装方法](https://liteloaderqqnt.github.io/guide/plugins.html)）
+也可以clone或下载zip文件解压，保留文件夹结构（文件夹名称为`插件名`，内容为github上的内容），将文件夹移动至`LiteLoaderQQNT数据目录/plugins/`下面，重启QQNT即可。
 
 **版本不兼容提示**：从0.1.2起，插件已适配1.0版本以上`LiteLoaderQQNT`框架，同时不再兼容旧版框架，请遵循[安装方法](https://liteloaderqqnt.github.io/guide/install.html)更新框架。
 
+本插件有2个功能：
+1. NTQQ中点击不明链接，QQ会强制跳转到拦截页。安装本插件后，都会直达目标站点，不会再显示风险拦截页。当然，如果点击危险链接，风险自负。
+2. 如果你不想用默认浏览器打开连接，你可以配置自定义浏览器打开。
 
-
-
-
-也可以clone或下载zip文件解压，保留文件夹结构（文件夹名称为`插件名`，内容为github上的内容），将文件夹移动至`LiteLoaderQQNT数据目录/plugins/`下面，重启QQNT即可。
-
-随后点击任何连接，都会直达目标站点，不会再显示风险拦截页。
-
-当然，如果点击危险链接，风险自负。
+如果不想用默认浏览器打开连接，你也可以前往 `LiteLoaderQQNT数据目录\directly_jump` 中找到 `config.json` 配置文件（至少在安装插件后开过一次QQ才会有这个文件），修改其中的 `browser` 字段为你的浏览器可执行文件完整路径（例如 `C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe` ，注意路径要写双斜杠，或者反斜杠 `/` ），配置此项后，点击任何链接都将采用指定浏览器打开。
 
 ## 原理
 
--- main.js
+++ main.js
@@ -1,27 +1,49 @@
+const exec = require("child_process").exec;
+const fs = require("fs");
+var defaultBrowser = LiteLoader.api.config.get("directly_jump", {
+  browser: "",
+});
+
 function onBrowserWindowCreated(window) {
-    const original_send =
-        (window.webContents.__qqntim_original_object &&
-            window.webContents.__qqntim_original_object.send) ||
-        window.webContents.send;
+  const original_send =
+    (window.webContents.__qqntim_original_object &&
+      window.webContents.__qqntim_original_object.send) ||
+    window.webContents.send;
 
-    const patched_send = function (channel, ...args) {
-        if (args.length >= 2) {
-            if (args.some((item) => item && item.checkUrlsResult != null)) {
-                args[1].checkUrlsResult.forEach((i) => {
-                    i.result = 1;
-                    i.jumpResult = 11;
-                    i.jumpUrl = "";
-                });
-            }
+  const patched_send = function (channel, ...args) {
+    if (args.length >= 2) {
+      if (args.some((item) => item && item.checkUrlsResult != null)) {
+        console.log(fs.existsSync(defaultBrowser.browser));
+        if (
+          (defaultBrowser?.browser ?? "") != "" &&
+          fs.existsSync(defaultBrowser.browser)
+        ) {
+          var cmd =
+            '"' +
+            defaultBrowser.browser +
+            '"' +
+            ' "' +
+            args[1].checkUrlsResult[0].url +
+            '"';
+          exec(cmd);
+          args.pop();
+        } else {
+          args[1].checkUrlsResult.forEach((i) => {
+            i.result = 1;
+            i.jumpResult = 11;
+            i.jumpUrl = "";
+          });
         }
-        return original_send.call(window.webContents, channel, ...args);
-    };
+      }
+    }
+    return original_send.call(window.webContents, channel, ...args);
+  };
 
-    if (window.webContents.__qqntim_original_object)
-        window.webContents.__qqntim_original_object.send = patched_send;
-    else window.webContents.send = patched_send;
+  if (window.webContents.__qqntim_original_object)
+    window.webContents.__qqntim_original_object.send = patched_send;
+  else window.webContents.send = patched_send;
 }
 
 module.exports = {
-    onBrowserWindowCreated
+  onBrowserWindowCreated,
 };
